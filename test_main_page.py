31c498b7915dc27f39491a8aca12f0f5b0474777
unknown <slavyansk2001@mail.ru>
2021-11-11 19:32:16 +0300

    transition to login page link from test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -10,8 +10,10 @@ def test_guest_can_go_to_login_page(browser):
     page.should_be_login_link()
 
 
-def test_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
-    page.should_be_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
