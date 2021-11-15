f4bfe6ba8d504a12560bf06a2fc149a5588bed85
unknown <slavyansk2001@mail.ru>
2021-11-15 21:45:42 +0300

    preparing the code for preview

--- test_product_page.py
+++ test_product_page.py
@@ -1,41 +1,64 @@
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
-from .pages.base_page import BasePage
 import pytest
 import time
 
-"""
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
-    link = f"{link}"
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_add_to_basket_message()
     page.should_be_price_equal_in_basket()
-    time.sleep(0)
-"""
 
-'''
-@pytest.mark.xfail(reason="success message is presented after add to basket")
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_be_basket_empty()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.add_to_basket()
-    time.sleep(0)
-    page.should_not_be_success_message()
+    page.go_to_login_page()
+
+
+@pytest.mark.new_user
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        new_user = LoginPage(browser, link)
+        new_user.open()
+        new_user.go_to_login_page()
+        email = str(time.time()) + "@fakemail.org"
+        new_user.register_new_user(email, email)
+        new_user.should_be_authorized_user()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.should_be_add_to_basket_message()
+        page.should_be_price_equal_in_basket()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
 
 
 def test_guest_cant_see_success_message(browser):
@@ -61,45 +84,11 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+@pytest.mark.xfail(reason="success message is presented after add to basket")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.open()
-    page.go_to_login_page()
-
-
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-    page = BasketPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    page.should_be_basket_empty()
-'''
-
-
-@pytest.mark.new_user
-class TestUserAddToBasketFromProductPage():
-    @pytest.fixture(scope="function", autouse=True)
-    def setup(self, browser):
-        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-        new_user = LoginPage(browser, link)
-        new_user.open()
-        new_user.go_to_login_page()
-        email = str(time.time()) + "@fakemail.org"
-        new_user.register_new_user(email, email)
-        new_user.should_be_authorized_user()
-
-    def test_user_can_add_product_to_basket(self, browser):
-        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-        page = ProductPage(browser, link)
-        page.open()
-        page.add_to_basket()
-        #page.solve_quiz_and_get_code()
-        page.should_be_add_to_basket_message()
-        page.should_be_price_equal_in_basket()
-
-    def test_user_cant_see_success_message(self, browser):
-        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-        page = ProductPage(browser, link)
-        page.open()
-        page.should_not_be_success_message()
+    page.add_to_basket()
+    time.sleep(0)
+    page.should_not_be_success_message()
