9f5705a2a098b5bf5e83d27ff7820d947b2b51a1
unknown <slavyansk2001@mail.ru>
2021-11-16 20:03:59 +0300

    solve test_guest_can_add_product_to_basket, delete solve_quiz

--- test_product_page.py
+++ test_product_page.py
@@ -11,7 +11,6 @@ def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
-    time.sleep(10)
     page.should_be_add_to_basket_message()
     page.should_be_price_equal_in_basket()
 
